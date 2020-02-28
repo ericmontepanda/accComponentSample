f3ff8dbd1be90254ec4d716b93f237da8e2c5793
eric montelibano <eric.montelibano@pwc.com>
2020-02-28 08:37:09 -0500

    update

--- force-app/main/default/lwc/accountComponent/accountComponent.js
+++ force-app/main/default/lwc/accountComponent/accountComponent.js
@@ -46,8 +46,9 @@ export default class AccountComponent extends NavigationMixin(LightningElement) 
             this.name = this.acccount.fields.Name.value;
             this.phone = this.acccount.fields.Phone.value;
             this.rating = this.acccount.fields.Rating.value;
-            console.log('----' + this.account.fields.Type.value)
-            this.accType = this.account.fields.Type.value;
+            console.log('----' + this.account.fields.Type.value);
+            this.accType = this.account.fields.Type;
+
             console.log('this... account ' + JSON.stringify(this.account));
         } else if (error) {
             let message = 'Unknown error';
